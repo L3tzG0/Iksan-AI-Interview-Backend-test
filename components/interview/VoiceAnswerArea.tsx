30021976fde5fddf7f41f5ca25391607ac9878f3
Muhammad Brillian <brillian354@gmail.com>
2025-12-31 11:51:07 +0700

    feat: adjust answer box width

--- components/interview/VoiceAnswerArea.tsx
+++ components/interview/VoiceAnswerArea.tsx
@@ -235,7 +235,7 @@ const VoiceAnswerArea: React.FC<VoiceAnswerAreaProps> = ({
             )}
 
             {!isOddQuestion && inputMode === "text" && (
-                <div className={`flex flex-col gap-4 h-full w-full px-24`}>
+                <div className={`flex flex-col gap-4 h-full w-full`}>
                     <div className="relative min-h-[260px] max-h-[440px]">
                         {/* {isOddQuestion && (
                         <>
